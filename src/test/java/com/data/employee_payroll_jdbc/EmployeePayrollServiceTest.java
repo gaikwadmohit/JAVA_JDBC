61b94ee65c5b12c23c487deea202686d8fdeb525
unknown <mohit16gaikwad@gmail.com>
2022-05-08 17:57:48 +0530

    Added: Added: code to select data from database

--- src/test/java/com/data/employee_payroll_jdbc/EmployeePayrollServiceTest.java
+++ src/test/java/com/data/employee_payroll_jdbc/EmployeePayrollServiceTest.java
@@ -1,12 +1,31 @@
 package com.data.employee_payroll_jdbc;
 
+import java.sql.SQLException;
+
 import org.junit.Test;
 
 public class EmployeePayrollServiceTest {
-
+	/**
+	 * test case to check database connected or not
+	 * 
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
 	@Test
-	public void givenEmployeePayrollDatabase_ShouldConnectToDatabase() {
+	public void givenEmployeePayrollDatabase_ShouldConnectToDatabase() throws ClassNotFoundException, SQLException {
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
 		employeePayrollService.checkDatabaseConnection();
+	}
+
+	/**
+	 * test cases to test data retrieval from database
+	 * 
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	@Test
+	public void givenEmployeePayrollDatabase_ShouldRetrieveData() throws ClassNotFoundException, SQLException {
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		employeePayrollService.selectData();
 	}
 }
